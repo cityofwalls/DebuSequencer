321846393df991ed9f44aa652ce671408abec935
Jesse <acityofwalls@gmail.com>
2019-04-04 11:53:28 -0700

    Update system structure

---
+++ midistuff.py
@@ -0,0 +1,222 @@
+from music21 import *
+
+INST = instrument.Instrument
+MET = tempo.MetronomeMark
+KEY = key.Key
+TIME_SIG = meter.TimeSignature
+
+def transpose_to_c(note, orig_key):
+    if not orig_key:
+        return note
+
+    # Traspositions: Each note gets transposed to it's scale degree in C
+    # First operate on notes from 'even' keys (number of sharps or flats)
+    if orig_key % 2 != 0:
+        if orig_key > 0:
+            t_pose = (orig_key + 6) % 12
+            note.transpose(t_pose)
+            return note
+        else:
+            if orig_key > -6:
+                t_pose = orig_key + 6
+                note.transpose(-t_pose)
+                return note
+            else:
+                note.transpose(-11)
+                return note
+    # Otherwise we have an 'odd' number of keys, first look at flats
+    elif orig_key < 0:
+        if orig_key > -5:
+            t_pose = 16 // orig_key
+            note.transpose(t_pose)
+            return note
+        else:
+            note.transpose(t_pose)
+            return note
+    # We don't change 'even' sharps
+    return note
+
+def is_new_measure_info(e):
+    return (isinstance(e, INST) or
+            isinstance(e, MET)  or
+            isinstance(e, KEY)  or
+            isinstance(e, TIME_SIG))
+
+def get_element_type(e):
+    if isinstance(e, INST):
+        return 'inst'
+    if isinstance(e, MET):
+        return 'met'
+    if isinstance(e, KEY):
+        return 'key'
+    if isinstance(e, TIME_SIG):
+        return 'time'
+    if isinstance(e, note.Rest):
+        return 'rest'
+    if isinstance(e, note.Note):
+        return 'note'
+    if isinstance(e, chord.Chord):
+        return 'chord'
+    return ''
+
+def get_rest(e):
+    dur = e.duration.quarterLength
+    if dur > 4.0:
+        dur = 4.0
+    return '&&', '0', dur
+
+def get_note(e, k):
+    c = transpose_to_c(e, k)
+    name = str(c.pitch.name) + str(c.pitch.octave)
+    vel = c.volume.velocity
+    dur = c.duration.quarterLength
+    return name, vel, dur
+
+def get_chord(e, k):
+    for note in e.pitches:
+        note = transpose_to_c(note, k)
+    cur_chord = []
+    for i in range(len(e.pitches)):
+        cur_chord.append(str(e.pitches[i].name) + str(e.pitches[i].octave))
+    cur_chord = [tuple(cur_chord)]
+    cur_chord.append(e.volume.velocity)
+    cur_chord.append(e.duration.quarterLength)
+    return cur_chord
+
+def save(seqs, cur_seq, cur_inst, cur_met, cur_key, cur_time):
+    sequence_to_add = [cur_seq]
+    if cur_inst:
+        sequence_to_add.append(cur_inst)
+    else:
+        sequence_to_add.append('Piano')
+
+    if cur_met:
+        sequence_to_add.append(cur_met)
+    else:
+        sequence_to_add.append(90.0)
+
+    if cur_key:
+        sequence_to_add.append(cur_key)
+    else:
+        sequence_to_add.append(0)
+
+    if cur_time:
+        sequence_to_add.append(cur_time)
+    else:
+        sequence_to_add.append('4/4')
+
+    seqs.append(sequence_to_add)
+    return seqs
+
+def get_sequences(lavender):
+    sequences = []
+    for i in range(len(lavender.parts)):
+        part = lavender.parts[i]
+        cur_sequence = []
+        cur_inst, cur_met, cur_key, cur_time = None, None, None, None
+        new_sequence = False
+        for element in part.recurse():
+            e = get_element_type(element)
+            if is_new_measure_info(element):
+                if new_sequence:
+                    sequences = save(sequences, cur_sequence, cur_inst, cur_met, cur_key, cur_time)
+                    new_sequence = False
+                    cur_sequence = []
+
+                if e == 'inst':
+                    cur_inst = element.instrumentName
+                if e == 'met':
+                    cur_met = element.number
+                if e == 'key':
+                    cur_key = element.sharps
+                if e == 'time':
+                    cur_time = element.ratioString
+
+            if e == 'rest':
+                new_sequence = True
+                cur_sequence.append(get_rest(element))
+            if e == 'note':
+                new_sequence = True
+                cur_sequence.append(get_note(element, cur_key))
+            if e == 'chord':
+                new_sequence = True
+                cur_sequence.append(get_chord(element, cur_key))
+
+        sequences = save(sequences, cur_sequence, cur_inst, cur_met, cur_key, cur_time)
+    return sequences
+
+def mus_seq_to_data(sequences):
+    """ sequences: A sequence of music events (notes, chords, rests)
+
+        Converts music events to a numpy array of data points containing pitch information
+        (grouped in a tuple if there is more than a single pitch), velocity, and duration
+        about the current note or chord, and other general information about the current meter.
+
+        Returns seq, conversions: the sequence of data points converted into categorical data
+        and a list of conversions to translate data back into note information. """
+    seq_full = []
+    for i in range(len(sequences)):
+        current_seq = sequences[i] # [(pitch(es), vel, dur), inst, tempo, key, time_sig]
+
+        # Unpack all additional sequence information
+        current_inst = current_seq[1]
+        current_tempo = current_seq[2]
+        current_key = current_seq[3]
+        current_time_sig = current_seq[4]
+
+        # Unpack note/chord data and formulate into data points
+        pitch_data = current_seq[0]
+        for j in range(len(pitch_data)):
+            # Grab information specific to the note or chord we're looking at
+            pitches = pitch_data[j][0]
+            velocity = pitch_data[j][1]
+            duration = pitch_data[j][2]
+
+            # Combine pitches, tempo, key, time sig, velocity, duration, inst into a single data point
+            data_point = (
+                pitches,            # [0]
+                current_tempo,      # [1]
+                current_key,        # [2]
+                current_time_sig,   # [3]
+                velocity,           # [4]
+                duration,           # [5]
+                current_inst        # [6]
+            )
+
+            seq_full.append(data_point)
+
+    return seq_full
+
+def data_to_mus_seq(data, factors, num_voices):
+    debusequence = stream.Stream()
+    for _ in range(num_voices):
+        voice = stream.Stream()
+        offset = 0.0
+        for i in range(len(data)):
+            pred = factors[data[i]]
+            if pred[0] == '&&':
+                new_note = note.Rest()
+            elif isinstance(pred[0], tuple):
+                pitches = []
+                for pitch in pred[0]:
+                    pitches.append(pitch)
+                new_note = chord.Chord(pitches)
+            else:
+                new_note = note.Note(pred[0])
+
+            new_note.volume = volume.Volume(velocity=int(pred[1]))
+            new_note.duration = duration.Duration(pred[5])
+            new_note.offset = offset
+            offset += new_note.duration.quarterLength
+            new_note.storedInstrument = pred[6]
+
+            voice.insert(new_note)
+
+        debusequence.insert(voice)
+
+    return debusequence
+
+def write_to_midi(lavender, filename='peepthis'):
+    lavender.write('midi', fp=f'{filename}.mid')
+    peep = converter.parse(f'./{filename}.mid')
+    print(f'Written to ./{filename}.mid')
---
+++ modelmaker.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import numpy as np
+from numpy import array
+from random import choice
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import Dense
+from keras.layers import Activation
+from keras.utils import np_utils
+import midistuff
+
+class Brain:
+    """  """
+    def __init__(self,
+                 data,
+                 train_seq_length=100,
+                 num_lstm_layers=2,
+                 num_dense_layers=1,
+                 lstm_nodes=512,
+                 dense_nodes=256,
+                 dropout_rate=0.3,
+                 temperature=1.0,
+                 generate_length=100,
+                 num_voices=1,
+                 act='softmax',
+                 loss_func='sparse_categorical_crossentropy',
+                 opt='rmsprop'):
+        """  """
+        self.train_seq_length = train_seq_length
+        self.X, self.y = self.data_to_X_y(data)
+
+        self.temperature = temperature
+        self.generate_length = generate_length
+        self.num_voices = num_voices
+
+        self.model = Sequential()
+        self.model.add(LSTM(
+                        lstm_nodes,
+                        input_shape=(self.X.shape[1], self.X.shape[2]),
+                        return_sequences=True))
+        self.model.add(Dropout(dropout_rate))
+
+        for i in range(1, num_lstm_layers):
+            self.model.add(LSTM(lstm_nodes, return_sequences=True))
+            self.model.add(Dropout(dropout_rate))
+
+        for i in range(num_dense_layers):
+            self.model.add(Dense(dense_nodes))
+            self.model.add(Dropout(dropout_rate))
+
+        self.model.add(Dense(self.vocab))
+        self.model.add(Activation(act))
+        self.model.compile(loss=loss_func, optimizer=opt)
+
+    def data_to_X_y(self, data):
+        """ Given a dataset of music21 objects, get input set (X) and label (y)
+            Parameters:  data,               a list of lists where each internal list is a sequence
+                                             of music21 objects representing a voice from a midi file.
+
+            Returns:     X,                  a list of lists where each internal list is a sequence
+                                             from data of length train_seq_length.
+
+                         y,                  a list of labels (the next value for a given sequence). """
+
+        # Flatten data into a single sequence and factorize
+        # This unifies the factors (categories) across all sequences
+        flat_data = []
+        for i in range(len(data)):
+            for j in range(len(data[i])):
+                flat_data.append(data[i][j])
+        _, factors = pd.factorize(flat_data)
+        self.factors = factors
+        self.vocab = len(factors)
+
+        cat_data = []
+        for i in range(len(data)):
+            cat_data.append([])
+            for j in range(len(data[i])):
+                idx = list(factors).index(data[i][j])
+                cat_data[i].append(idx)
+
+        seq_in, seq_out = [], []
+        for i in range(len(cat_data)):
+            current_seq = cat_data[i]
+            for j in range(len(current_seq) - self.train_seq_length):
+                seq_in.append(current_seq[j:j+self.train_seq_length])
+                seq_out.append(current_seq[j+self.train_seq_length])
+
+        X, y = array(seq_in), array(seq_out)
+        X = X.reshape(len(X), 1, len(X[0]))
+        y = y.reshape(len(y), 1, 1)
+
+        # Get the seed for generate method
+        # self.seed = array(self.get_seed(cat_data, self.train_seq_length))
+        # self.seed = self.seed.reshape(1, 1, self.train_seq_length)
+
+        self.set_seed(cat_data)
+
+        return X, y
+
+    def set_seed(self, seq):
+        """ Given a categorical sequence, find a random seed sequence of length l. """
+        # seq_choice = choice(seq)
+        # while len(seq_choice) < self.train_seq_length:
+        #     seq_choice = choice(seqs)
+        #
+        # idx = seq_choice.index(choice(seq_choice[:-self.train_seq_length - 1]))
+        # seed = seq_choice[idx:idx+self.train_seq_length]
+        #
+        # seed = array(seed)
+        # self.seed = seed.reshape(1, 1, self.train_seq_length)
+
+        seed = []
+        for _ in range(self.train_seq_length):
+            seed.append(choice(range(self.vocab)))
+
+        seed = array(seed)
+        self.seed = seed.reshape(1, 1, self.train_seq_length)
+
+    def train(self, num_of_epochs=2):
+        self.model.fit(self.X, self.y, epochs=num_of_epochs, shuffle=False, verbose=1)
+
+    def sample(self, preds):
+        preds = np.asarray(preds).astype('float64')
+        preds = np.log(preds) / self.temperature
+        exp_preds = np.exp(preds)
+        preds = exp_preds / np.sum(exp_preds)
+        probas = np.random.multinomial(1, preds, 1)
+        return np.argmax(probas)
+
+    def generate(self):
+        predicted_sequence = []
+        cur_seq = self.seed
+        for i in range(self.generate_length * self.num_voices):
+            y_hat = self.model.predict(cur_seq, verbose=0)[0][0]
+            idx = self.sample(y_hat)
+            predicted_sequence.append(idx)
+            cur_seq = cur_seq[0][0][1:]
+            cur_seq = np.append(cur_seq, idx)
+            cur_seq = cur_seq.reshape(1, 1, self.train_seq_length)
+
+        #print(predicted_sequence)
+
+        return midistuff.data_to_mus_seq(predicted_sequence, self.factors, self.num_voices)
